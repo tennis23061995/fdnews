2dd86fee10c0339ffca8f78fa4f2181d25c727c5
Phan Thanh Tam <rogerphan0623@gmail.com>
2017-11-27 13:29:23 +0700

    a

--- dist/js/dropdownmenu.js
+++ dist/js/dropdownmenu.js
@@ -1,17 +1,19 @@
 $(function() {
-	$("nav.DropDownMenu > ul > li:has(ul)").click(function() {
-		if($(this).hasClass("click")) {
-			$(this).removeClass("click").removeClass("open");
+	$(".btn-submenu").click(function(e) {
+		e.preventDefault();
+		if($(this).parent().hasClass("click")) {
+			$(this).parent().removeClass("click").removeClass("open");
 		} else {
-			$(this).parent().children("li").removeClass("click").removeClass("open");
-			$(this).addClass("click").addClass("open");
+			$(this).children("li").removeClass("click").removeClass("open");
+			$(this).parent().addClass("click").addClass("open");
 		}
-	}).mouseenter(function() {
+	});
+	$("nav.DropDownMenu > ul > li:has(ul)").mouseenter(function() {
 		if($(this).css("float") == "left") if(!$(this).parent().children("li").hasClass("click")) $(this).addClass("open");
 	}).mouseleave(function(){
 		if($(this).css("float") == "left") if(!$(this).hasClass("click")) $(this).removeClass("open");
 	});
-	
+
 	/*Mobile version opening*/
 	$("body > header > div > svg").click(function() {
 		$("body > header > div > nav").toggleClass("open");
